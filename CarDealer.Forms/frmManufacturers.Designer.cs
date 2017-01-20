a975ebf5cfb4f85dfe538ba6303778186eef2350
Aula Randstad <Aula Randstad@PC2225>
2017-01-20 10:55:20 +0100

    Added databinding

--- CarDealer.Forms/frmManufacturers.Designer.cs
+++ CarDealer.Forms/frmManufacturers.Designer.cs
@@ -33,6 +33,10 @@
             this.manifactureBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.btnClose = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.websiteDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.carModelsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvManufacturers)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.manifactureBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -40,7 +44,14 @@
             // dgvManufacturers
             // 
             this.dgvManufacturers.AllowUserToOrderColumns = true;
+            this.dgvManufacturers.AutoGenerateColumns = false;
             this.dgvManufacturers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvManufacturers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.websiteDataGridViewTextBoxColumn,
+            this.carModelsDataGridViewTextBoxColumn});
+            this.dgvManufacturers.DataSource = this.manifactureBindingSource;
             this.dgvManufacturers.Location = new System.Drawing.Point(13, 13);
             this.dgvManufacturers.Name = "dgvManufacturers";
             this.dgvManufacturers.Size = new System.Drawing.Size(363, 208);
@@ -70,6 +81,30 @@
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // websiteDataGridViewTextBoxColumn
+            // 
+            this.websiteDataGridViewTextBoxColumn.DataPropertyName = "Website";
+            this.websiteDataGridViewTextBoxColumn.HeaderText = "Website";
+            this.websiteDataGridViewTextBoxColumn.Name = "websiteDataGridViewTextBoxColumn";
+            // 
+            // carModelsDataGridViewTextBoxColumn
+            // 
+            this.carModelsDataGridViewTextBoxColumn.DataPropertyName = "CarModels";
+            this.carModelsDataGridViewTextBoxColumn.HeaderText = "CarModels";
+            this.carModelsDataGridViewTextBoxColumn.Name = "carModelsDataGridViewTextBoxColumn";
+            // 
             // frmManufacturers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -93,5 +128,9 @@
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.BindingSource manifactureBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn websiteDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn carModelsDataGridViewTextBoxColumn;
     }
 }
